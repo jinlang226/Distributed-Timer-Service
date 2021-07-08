16b55f8f1e1f4dff6127e95ed4947c8c8a208330
Jinlang Wang <54641709+jinlang226@users.noreply.github.com>
2021-07-08 18:41:19 +0800

    Create job.go

---
+++ src/timeWheel/job.go
@@ -0,0 +1,15 @@
+package timeWheel
+
+import (
+	"fmt"
+	"time"
+)
+
+func TimeWheelDefaultJob(key interface{}) {
+	fmt.Println(fmt.Sprintf("%v This is a timewheel job with key: %v", time.Now().Format(time.RFC3339), key))
+}
+
+func TaskJob(key interface{}) {
+	fmt.Println(fmt.Sprintf("%v This is a task job with key: %v", time.Now().Format(time.RFC3339), key))
+}
+
