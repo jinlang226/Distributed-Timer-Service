49df919c9b928de2d0a90c5b4df3f12ffb84e00a
Jinlang Wang <54641709+jinlang226@users.noreply.github.com>
2021-07-08 18:49:20 +0800

    Update main.go

--- src/main/main.go
+++ src/main/main.go
@@ -1,9 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("hello")
+
+
+	//读进来 CSV文件，存到tasks里面
+	//var tasks map[int]int
+	//平均的分成5份，分给每个机器
+
+	//
+
+	//
 }
 
 
