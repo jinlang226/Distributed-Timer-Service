f5a21e4408585480c1fa71dc7829485ec139ce2e
Jinlang Wang <54641709+jinlang226@users.noreply.github.com>
2021-07-09 11:59:50 +0800

    struc

--- src/main/main.go
+++ src/main/main.go
@@ -7,14 +7,15 @@ import (
 func main() {
 	fmt.Println("hello")
 
-
-	//读进来 CSV文件，存到tasks里面
+	//read in csv document, save it to tasks
 	//var tasks map[int]int
-	//平均的分成5份，分给每个机器
+	//evenly distribute the document to 5 machines
 
 	//
+}
 
-	//
+func init() {
+	// init a cli app
 }
 
 
